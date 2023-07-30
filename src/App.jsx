a73558525ebf6e61c31deff8d12ecfc9314fbce2
aelng <elnagmiali@gmail.com>
2023-07-30 11:02:45 -0400

    first revision

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Particle from './components/Particle'
 import Topnav from './components/Topnav'
 import About from './components/About'
 import Projects from './components/Projects'
+import './components/About.scss'
 
 export default function App() {
 
@@ -12,6 +13,13 @@ export default function App() {
       <Topnav/>
       <About/>
       <Projects/>
+      <footer className='foot'>
+        <p>Copyright @ 2023: Ali Elnagmi</p>
+
+        <p>
+          <p>Made with React and Tailwind</p>
+        </p>
+      </footer>
     </>
   )
 }
